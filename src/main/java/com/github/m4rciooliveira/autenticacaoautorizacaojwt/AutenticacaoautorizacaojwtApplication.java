9014697c36a24bb076771e2a9fb8c22a47fbc32f
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2025-01-24 14:51:54 -0300

    first commit

---
+++ src/main/java/com/github/m4rciooliveira/autenticacaoautorizacaojwt/AutenticacaoautorizacaojwtApplication.java
@@ -0,0 +1,13 @@
+package com.github.m4rciooliveira.autenticacaoautorizacaojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoautorizacaojwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoautorizacaojwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/m4rciooliveira/autenticacaoautorizacaojwt/AutenticacaoautorizacaojwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m4rciooliveira.autenticacaoautorizacaojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoautorizacaojwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
